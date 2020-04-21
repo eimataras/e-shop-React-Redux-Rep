954a229af7d6bacba396146a30a51076e1e92a5e
Eimantas Tarasevičius <eimataras@gmail.com>
2020-04-21 15:08:52 +0300

    transformed SignIn.js from class to const component using useState hook for collecting user credentials

--- src/components/app/auth/SignIn.js
+++ src/components/app/auth/SignIn.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import Button from "@material-ui/core/Button";
 import {Container} from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
@@ -18,61 +18,55 @@ const mapDispatchToProps = (dispatch) => bindActionCreators({
     postLogin: (username, password, props) => postLogin(username, password, props),
 }, dispatch);
 
-class SignIn extends Component {
-    state = {
-        username: '',
-        password: '',
-    };
+const SignIn = props => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value
-        });
-    };
 
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        this.props.postLogin(this.state.username, this.state.password, this.props);
-        this.setState({
-            username: '',
-            password: ''
-        })
+        props.postLogin(username, password, props);
+        setUsername('');
+        setPassword('')
     };
 
-    render() {
-        return (
-            <Container fixed maxWidth="xs">
-                <Paper className="padding">
-                    <div align="center">
-                        <form autoComplete="off">
-                            <h1>Log In:</h1>
-                            {this.props.currentUser.error ? (
-                                <h5 style={{color: "red"}}>Wrong username or password!</h5>) : ''}
-                            <TextField id="username" name="username" type="username" variant="outlined"
-                                       label="Username"
-                                       value={this.state.username} onChange={this.handleChange}/>
-                            <TextField id="password" name="password" type="password" variant="outlined"
-                                       label="Password"
-                                       value={this.state.password} onChange={this.handleChange}/>
-                            <br/>
-                            <div>
-                                <input type="checkbox" id="remember-me" name="remember-me"/>
-                                <label htmlFor="remember-me">Remember me?</label>
-                            </div>
-                            <br/>
-                            <div className="padding">
-                                <Button variant="contained" color="primary" type="submit"
-                                        onClick={this.handleSubmit}>Login</Button>
-                            </div>
-                        </form>
-                    </div>
-                </Paper>
-            </Container>
-        )
-    }
-}
+    const handleChangeUsername = e => {
+        setUsername(e.target.value)
+    };
 
-export default compose(
-    withRouter,
-    connect(mapStateToProps, mapDispatchToProps)
-)(SignIn);
+    const handleChangePassword = e => {
+        setPassword(e.target.value)
+    };
+
+    return (
+        <Container fixed maxWidth="xs">
+            <Paper className="padding">
+                <div align="center">
+                    <form autoComplete="off">
+                        <h1>Log In:</h1>
+                        {props.currentUser.error ? (
+                            <h5 style={{color: "red"}}>Wrong username or password!</h5>) : ''}
+                        <TextField autoFocus id="username" name="username" type="username" variant="outlined"
+                                   label="Username"
+                                   value={username} onChange={handleChangeUsername}/>
+                        <TextField id="password" name="password" type="password" variant="outlined"
+                                   label="Password"
+                                   value={password} onChange={handleChangePassword}/>
+                        <br/>
+                        <div>
+                            <input type="checkbox" id="remember-me" name="remember-me"/>
+                            <label htmlFor="remember-me">Remember me?</label>
+                        </div>
+                        <br/>
+                        <div className="padding">
+                            <Button variant="contained" color="primary" type="submit"
+                                    onClick={handleSubmit}>Login</Button>
+                        </div>
+                    </form>
+                </div>
+            </Paper>
+        </Container>
+    )
+};
+
+export default compose(withRouter, connect(mapStateToProps, mapDispatchToProps))(SignIn);
